0b2ef523fc9984db05a4f872993987fec6de6f44
stark <akshaystark3000@gmail.com>
2026-06-25 20:15:18 +0530

    added backend rewrite proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://43.205.199.22/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
