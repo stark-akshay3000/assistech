6000c5b31cb2b6bd13718d2d218ad1305c83abd4
stark <akshaystark3000@gmail.com>
2026-06-26 20:08:41 +0530

    fixed mixed content issue

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL
+  ? "/backend"
+  : "http://localhost:8000";
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  baseURL,
 });
